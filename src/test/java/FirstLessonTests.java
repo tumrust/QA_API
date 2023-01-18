8d650d37a835c44b22ca4be1a4d585f921933563
tumrust <rustem.stalker@gmail.com>
2023-01-18 17:46:03 +0400

    refactor

--- src/test/java/FirstLessonTests.java
+++ src/test/java/FirstLessonTests.java
@@ -2,8 +2,11 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-public class GetTextTest {
-
+public class FirstLessonTests {
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello from Rustem");
+    }
     @Test
     public void testGetText() {
         Response response = RestAssured
