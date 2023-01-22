df5530d6a078660d9e4d43367a3448f410ae6a2e
tumrust <rustem.stalker@gmail.com>
2023-01-22 13:53:50 +0400

    add test passwordSelectionTests

--- src/test/java/SecondLessonTests.java
+++ src/test/java/SecondLessonTests.java
@@ -8,13 +8,14 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.apache.http.client.methods.RequestBuilder.post;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SecondLessonTests {
 
     @Test
     public void testGetJsonHomework() {
-        Map<String ,String> messages = new HashMap<>();
+        Map<String, String> messages = new HashMap<>();
 
         JsonPath response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_json_homework")
@@ -100,4 +101,38 @@ public class SecondLessonTests {
         assertEquals("42", params.get("result"), "Unexpected result");
     }
 
+    @Test
+    public void passwordSelection() {
+        String getSecretPassword = "https://playground.learnqa.ru/ajax/api/get_secret_password_homework";
+        String checkCookie = "https://playground.learnqa.ru/ajax/api/check_auth_cookie";
+
+        Map<String, String> data = new HashMap<>();
+        data.put("login", "super_admin");
+        data.put("password", "secret_password");
+
+        String[] passwords = new String[]{ "1234", "solo", "12345", "admin", "hello", "jesus", "login", "ninja", "000000", "123qwe", "111111", "121212", "123123", "123456", "555555", "654321", "666666", "696969", "888888", "abc123", "access", "ashley", "azerty", "batman", "donald", "dragon", "flower", "hottie", "lovely", "loveme", "master", "monkey", "qazwsx", "qwerty", "shadow", "1234567", "7777777", "charlie", "freedom", "letmein", "michael", "mustang", "welcome", "1q2w3e4r", "1qaz2wsx", "12345678", "!@#$%^&*", "aa123456", "adobe123", "baseball", "football", "iloveyou", "passw0rd", "password", "princess", "starwars", "sunshine", "superman", "trustno1", "whatever", "zaq1zaq1", "123456789", "password1", "photoshop", "qwerty123", "1234567890", "qwertyuiop", "12345", "111111", "123123", "654321", "abc123", "ashley", "bailey", "dragon", "master", "monkey", "qazwsx", "qwerty", "shadow", "1234567", "letmein", "michael", "Football", "baseball", "iloveyou", "passw0rd", "sunshine", "superman", "trustno1", "123456789"};
+
+
+        for (int i = 0; i < passwords.length; i++) {
+            data.put("login", "super_admin");
+            data.put("password", passwords[i]);
+
+            Response checkPassword = RestAssured
+                    .given()
+                    .body(data)
+                    .post(getSecretPassword)
+                    .andReturn();
+
+            String auth_cookie = checkPassword.getCookie("auth_cookie");
+
+            Response checkAuth = RestAssured
+                    .given()
+                    .cookie(auth_cookie)
+                    .get(checkCookie)
+                    .andReturn();
+            System.out.println(data.get("password") + checkAuth.getBody().asString());
+            if (checkAuth.body().asString().equals("You are authorized"))
+                break;
+        }
+    }
 }
