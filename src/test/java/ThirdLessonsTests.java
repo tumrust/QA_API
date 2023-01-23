4ae073724e6f7a82545a792164f70086f6b8bf56
tumrust <rustem.stalker@gmail.com>
2023-01-23 19:14:57 +0400

    add test getCookieTest

--- src/test/java/ThirdLessonsTests.java
+++ src/test/java/ThirdLessonsTests.java
@@ -1,7 +1,10 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ThirdLessonsTests {
@@ -11,5 +14,13 @@ public class ThirdLessonsTests {
     String phrase = new String();
 
     assertTrue(phrase.length() >= 15,"The phrase is shorter than 15 symbols");
+    }
+
+    @Test
+    public void getCookieTest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        assertEquals("hw_value",response.getCookie("HomeWork"),"Unexpected cookie value");
     }
 }
