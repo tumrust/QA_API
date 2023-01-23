2611199d8739d91fa6d65285df15c172c632ff19
tumrust <rustem.stalker@gmail.com>
2023-01-23 23:18:30 +0400

    copypaste from lesson

---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRegisterTest extends BaseTestCase {
+
+    @Test
+    public void testCreateUserWithExistingEmail() {
+        String email = "vinkotov@example.com";
+
+        Map <String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData.put("password", "123");
+        userData.put("username", "learnqa");
+        userData.put("firstName", "learnqa");
+        userData.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth,400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
+    }
+
+    @Test
+    public void testCreateUserSuccessfully() {
+        String email = DataGenerator.getRandomEmail();
+
+        Map <String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData.put("password", "123");
+        userData.put("username", "learnqa");
+        userData.put("firstName", "learnqa");
+        userData.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth,200);
+        Assertions.assertJsonHasKey(responseCreateAuth,"id");
+    }
+}
